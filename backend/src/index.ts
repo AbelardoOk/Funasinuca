0ef66ed9ceaa025ccff26a91ab941d56c8ea03a5
AbelardoOk <Abelardoemailprivado@gmail.com>
2026-05-10 16:39:23 -0400

    Finalmente consertei essa desgraça

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import { Elysia } from 'elysia';
 
 const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
+app.get('/health', () => ({ status: 'ok' }));
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
